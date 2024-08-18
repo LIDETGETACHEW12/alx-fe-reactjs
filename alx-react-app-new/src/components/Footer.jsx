bf60beede45812fbd7676848bc7e4083323ffa1f
LidetGetachew <lidet.getachew@walletbirr.com>
2024-08-19 00:40:54 +0300

    all the commits have been taken care of

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Footer() {
+    return (
+        <div>
+            <footer style={{textAlign: 'center' }}>
+              <p style={{ fontWeight: 'light', color: 'navy'}}>© 2023 City Lovers</p>
+            </footer>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function MainContent() {
+    return (
+        <div>
+            <main  style={{ backgroundColor: 'gray', color: 'black', textAlign: 'center' }}>
+             <p style={{color:'navy'}}>I love to visit New York, Paris, and Tokyo.</p>
+            </main>
+        </div>
+    );
+}
+
+export default MainContent;

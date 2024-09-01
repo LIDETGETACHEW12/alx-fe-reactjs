d2d4fd899a273d1c93b5405689d9aa8a2f4e7f3d
LidetGetachew <lidet.getachew@walletbirr.com>
2024-09-01 13:40:02 +0300

    lastly

--- alx-react-app-new/src/components/Header.jsx
+++ alx-react-app-new/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 function Header() {
     return (
         <div>
-            <header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+            <header style={{ backgroundColor: 'red', color: 'yellow', textAlign: 'center' }}>
                       <h1>My Favorite Cities</h1>
             </header>
         </div>
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -2,8 +2,8 @@ import React from "react";
 function MainContent() {
     return (
         <div>
-            <main  style={{ backgroundColor: 'gray', color: 'black', textAlign: 'center' }}>
-             <p style={{color:'navy'}}>I love to visit New York, Paris, and Tokyo.</p>
+            <main  style={{ backgroundColor: 'white', color: 'black', textAlign: 'center' }}>
+             <p style={{color:'tomato'}}>I love to visit New York, Paris, and Tokyo.</p>
             </main>
         </div>
     );

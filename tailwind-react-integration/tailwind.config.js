fda831a8bf484e4f4e123ea9ac8e83832b253546
LidetGetachew <lidet.getachew@walletbirr.com>
2024-09-06 01:41:23 +0300

    tailwind

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", // Adjust the paths to match where your components are located
+    "./public/App.jsx",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+

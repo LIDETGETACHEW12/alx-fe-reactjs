86a963e43a192b4e68e22d4812f2134401fc679e
LidetGetachew <lidet.getachew@walletbirr.com>
2024-09-06 02:18:29 +0300

    tailwind compo6

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge:  [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html", 
+  ],
   darkMode: false,
   content: [],
   theme: {

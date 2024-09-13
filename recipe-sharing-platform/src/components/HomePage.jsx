f7a36a13a810ccb907c40eca79ceb0d68dd2381d
LidetGetachew <lidet.getachew@walletbirr.com>
2024-09-14 02:16:15 +0300

    this is the third

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import recipeData from '../data.json';
+import recipeData from "../data.json";
 
 const HomePage = () => {
   const [data, setData] = useState([]);
@@ -11,24 +11,22 @@ const HomePage = () => {
     setData(recipeData);
   }, []);
   return (
-    <div className="bg-white shadow-lg rounded-lg p-4 hover:shadow-xl hover:scale-105 transition transform duration-300">
-      <div className="container mx-auto p-4">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {data.map((single) => (
-            <div
-              key={single.id}
-              className="bg-white shadow-lg rounded-lg p-4 hover:shadow-xl transition-shadow duration-300"
-            >
-              <img
-                src={single.image}
-                alt={single.title}
-                className="w-full h-32 object-cover rounded-t-lg"
-              />
-              <h2 className="text-xl font-bold mt-2">{single.title}</h2>
-              <p className="text-gray-700">{single.summary}</p>
-            </div>
-          ))}
-        </div>
+    <div className="max-w-7xl mx-auto p-4 ">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 ">
+        {data.map((single) => (
+          <div
+            key={single.id}
+            className="bg-white shadow-md rounded-lg p-6  hover:scale-105 transition transform duration-300"
+          >
+            <img
+              src={single.image}
+              alt={single.title}
+              className="w-full h-40 object-cover rounded-t-lg"
+            />
+            <h2 className="text-lg font-bold mt-4 mb-2">{single.title}</h2>
+            <p className="text-gray-600 text-sm">{single.summary}</p>
+          </div>
+        ))}
       </div>
     </div>
   );

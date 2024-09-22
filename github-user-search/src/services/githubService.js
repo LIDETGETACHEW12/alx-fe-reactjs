2d1d401a63046ab41b73b9cc9479cfd20296cf97
LidetGetachew <lidet.getachew@walletbirr.com>
2024-09-23 01:06:19 +0300

    final1

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,20 +1,18 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://api.github.com/search/users?q={query}';
-
 export const fetchUserData = async ({ username, location, repos }) => {
   let query = '';
 
   // Construct the query string
   if (username) query += `user:${username}`;
   if (location) query += ` location:${location}`;
-  if ("minRepos") query += ` minRepos:>=${minRepos}`;
+  if (repos) query += ` repos:>=${repos}`;
 
   // Ensure the query string is properly formatted
   query = query.trim().replace(/\s+/g, '+'); // Replace spaces with '+'
 
   try {
-    const response = await axios.get(`${BASE_URL}?q=${query}`);
+    const response = await axios.get(`${process.env.REACT_APP_GITHUB_API_BASE_URL}?q=${query}`);
     return response.data.items; // The search API returns items array
   } catch (error) {
     throw new Error('Error fetching users');
